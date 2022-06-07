d15a1bddb8858af21762ea88199ddbba332343c3
Benjamin <benjamin.chen.hit@gmail.com>
2022-06-08 01:12:51 +0800

    🦄 refactor: analysis重构
    修改了一些方法

--- Analysis.py
+++ Analysis.py
@@ -3,13 +3,45 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 import pyecharts.options as opts
-from pyecharts.charts import Bar, Line, Page
+from pyecharts.charts import Bar, Line, Pie, Page
+
+
+excel_dir = './技术分汇总0606.xlsx'
 
 
 def get_week_list():
+    """获取2022/3/28到当前时间的周序号
+
+    Returns:
+        _type_: 开始的周序号，当前周序号
+    """
     start = pd.Timestamp('2022-03-28').week
     end = pd.Timestamp(datetime.now()).week
     return start, end
+
+
+def get_excel_groupby_result(excel_dir, sheet_name, base_groupby_col,
+                             groupby_col, target_col,
+                             base_groupby_col_time=True):
+    sheet = pd.read_excel(io=excel_dir,
+                          sheet_name=sheet_name, header=0)
+    # 日期类型处理
+    if(base_groupby_col_time):
+        sheet[base_groupby_col] = pd.to_datetime(sheet[base_groupby_col])
+        if(groupby_col):
+            sheet_local = sheet.groupby(
+                [np.int16(sheet[base_groupby_col].dt.isocalendar().week),
+                 sheet[groupby_col]]
+            )[target_col].agg('size').unstack()
+        else:
+            sheet_local = sheet.groupby(
+                [np.int16(sheet[base_groupby_col].dt.isocalendar().week)]
+            )[target_col].agg('size')
+    else:
+        sheet_local = sheet.groupby(
+            [sheet[base_groupby_col]]
+        )[target_col].agg('size')
+    return sheet_local
 
 
 def get_delay_list():
@@ -18,13 +50,15 @@ def get_delay_list():
     Returns:
         _type_: 延期任务 通过pandas生成的df
     """
-    delay_sheet = pd.read_excel(io='./技术分汇总0523.xlsx',
+    delay_sheet = pd.read_excel(io=excel_dir,
                                 sheet_name='jira-delay', header=0)
     delay_sheet['预估提测时间'] = pd.to_datetime(delay_sheet['预估提测时间'])
+    # 根据预计提测时间和难度级别分类
     delay_sheet_local = delay_sheet.groupby(
         [np.int16(delay_sheet['预估提测时间'].dt.isocalendar().week),
          delay_sheet['难度级别']]
     )['任务号'].agg('size').unstack()
+    # 根据预计提测时间分类
     delay_sheet_local_2 = delay_sheet.groupby(
         [np.int16(delay_sheet['预估提测时间'].dt.isocalendar().week)]
     )['任务号'].agg('size')
@@ -39,7 +73,7 @@ def get_bug_list():
     Returns:
         _type_: bug任务 通过pandas生成的df
     """
-    bug_sheet = pd.read_excel(io='./技术分汇总0523.xlsx',
+    bug_sheet = pd.read_excel(io=excel_dir,
                               sheet_name='bug', header=0)
     bug_sheet['创建日期'] = pd.to_datetime(bug_sheet['创建日期'])
     bug_sheet_local = bug_sheet.groupby(
@@ -80,20 +114,30 @@ def render_delay():
         _type_: 延期图表
     """
     # 延期图表
-    delay_df, delay_df_2 = get_delay_list()
+    delay_df = get_excel_groupby_result(excel_dir=excel_dir,
+                                        sheet_name='jira-delay',
+                                        base_groupby_col='预估提测时间',
+                                        groupby_col='难度级别',
+                                        target_col='任务号')
     delay_df = fill(delay_df)
+    delay_df_2 = get_excel_groupby_result(excel_dir=excel_dir,
+                                          sheet_name='jira-delay',
+                                          base_groupby_col='预估提测时间',
+                                          groupby_col='',
+                                          target_col='任务号')
     delay_df_2 = fill_2(delay_df_2)
     # 延期柱状图 按难易程度分类
     x_data = list(delay_df.index)
     y_data_easy = list(delay_df['简单'])
     y_data_normal = list(delay_df['一般'])
+    y_data_hard_low = list(delay_df['较难'])
     delay_bar = (
         Bar()
         .add_xaxis(x_data)
-        .add_yaxis('简单', y_data_easy)
+        .add_yaxis('较难', y_data_hard_low)
         .add_yaxis('一般', y_data_normal)
-        .set_global_opts(title_opts=opts.TitleOpts(title="延期汇总"))
-    )
+        .add_yaxis('简单', y_data_easy)
+        .set_global_opts(title_opts=opts.TitleOpts(title="延期汇总")))
     # 延期折线图 统计数量
     x_data = list(map(str, delay_df_2.index))
     y_data = list(map(int, delay_df_2.values))
@@ -120,6 +164,26 @@ def render_delay():
     delay_bar.overlap(delay_line)
     delay_bar.render('delay.html')
     return delay_bar
+
+
+def render_delay_pie():
+    delay_df_3 = get_excel_groupby_result(excel_dir=excel_dir,
+                                          sheet_name='jira-delay',
+                                          base_groupby_col='延期原因分类',
+                                          groupby_col=None,
+                                          target_col='任务号',
+                                          base_groupby_col_time=False)
+    delay_pie = (
+        Pie()
+        .add("",
+             [list(z) for z in zip(list(delay_df_3.index), list(delay_df_3))],
+             center=["40%", "50%"])
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title="延期原因分析"),
+            legend_opts=opts.LegendOpts(pos_right="2%", orient="vertical"))
+        .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
+    )
+    return delay_pie
 
 
 def render_bug():
@@ -182,8 +246,14 @@ def render():
         bug_df (_type_): 通过pandas生成的df
     """
     page = Page()
-    page.add(render_delay(), render_bug())
+    page.add(render_delay(),
+             render_delay_pie(),
+             render_bug())
     page.render('summary.html')
 
 
+# ret = get_excel_groupby_result(excel_dir=excel_dir, sheet_name='jira-delay',
+#                                base_groupby_col='预估提测时间', groupby_col='难度级别',
+#                                target_col='任务号')
+# print(ret)
 render()
